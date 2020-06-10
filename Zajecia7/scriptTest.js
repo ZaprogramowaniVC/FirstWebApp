d0efb97559f9ead903971ed987061cab5e447084
kamilxkruk@gmail.com <kamilxkruk@gmail.com>
2020-06-10 20:50:06 +0200

    Zajecia 8 koniec

--- Zajecia7/scriptTest.js
+++ Zajecia7/scriptTest.js
@@ -1,9 +1,10 @@
-TestFunction()
-TestFunctionWithInput('Parameter to input function')
-AddFunction(1,2);
-AddFunction('1',2);
+// TestFunction()
+// TestFunctionWithInput('Parameter to input function')
+// AddFunction(1,2);
+// AddFunction('1',2);
+// console.log( AddFunctionWithReturn(1,2) );
 
-console.log( AddFunctionWithReturn(1,2) );
+console.log(switchCaseFunction('1'));
 
 
 function TestFunction(){
@@ -20,4 +21,20 @@ function AddFunction(x, y){
 
 function AddFunctionWithReturn(x,y){
     return x+y;
+}
+
+
+function switchCaseFunction(onSwitch){
+    switch(onSwitch){
+        case '1':
+            return 'Jeden';
+        case '2':
+            return 'Dwa';
+        default:
+            return 'Nie znam tego numeru';
+    }
+}
+
+function IsNotEmpty(parameter){
+    return parameter !== "";
 }
